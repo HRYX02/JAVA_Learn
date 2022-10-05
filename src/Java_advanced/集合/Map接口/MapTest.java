af9461ade4b985102c0dd590a005b8689257e877
SxxStar <2291874995@qq.com>
2022-10-05 16:46:17 +0800

    JAVA学习的第三次commit

--- src/Java_advanced/集合/Map接口/MapTest.java
+++ src/Java_advanced/集合/Map接口/MapTest.java
@@ -2,9 +2,7 @@ package Java_advanced.集合.Map接口;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  *  一、Map的实现类的结构：
@@ -105,14 +103,66 @@ import java.util.Map;
  * @create 2022-10-03-12:33
  */
 public class MapTest {
+ /*
+ 元视图操作的方法：
+ Set keySet()：返回所有key构成的Set集合
+ Collection values()：返回所有value构成的Collection集合
+ Set entrySet()：返回所有key-value对构成的Set集合
+
+     */
+
+
+    @Test
+    public void test5(){
+        Map map = new HashMap();
+        map.put("AA",123);
+        map.put(45,1234);
+        map.put("BB",56);
+
+        //遍历所有的key集：keySet()
+        Set set = map.keySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println();
+        //遍历所有的value集：values()
+        Collection values = map.values();
+        for(Object obj : values){
+            System.out.println(obj);
+        }
+        System.out.println();
+        //遍历所有的key-value
+        //方式一：entrySet()
+        Set entrySet = map.entrySet();
+        Iterator iterator1 = entrySet.iterator();
+        while (iterator1.hasNext()){
+            Object obj = iterator1.next();
+            //entrySet集合中的元素都是entry
+            Map.Entry entry = (Map.Entry) obj;
+            System.out.println(entry.getKey() + "---->" + entry.getValue());
+
+        }
+        System.out.println();
+        //方式二：
+        Set keySet = map.keySet();
+        Iterator iterator2 = keySet.iterator();
+        while(iterator2.hasNext()){
+            Object key = iterator2.next();
+            Object value = map.get(key);
+            System.out.println(key + "=====" + value);
+
+        }
+
+    }
     /*
-元素查询的操作：
-Object get(Object key)：获取指定key对应的value
-boolean containsKey(Object key)：是否包含指定的key
-boolean containsValue(Object value)：是否包含指定的value
-int size()：返回map中key-value对的个数
-boolean isEmpty()：判断当前map是否为空
-boolean equals(Object obj)：判断当前map和参数对象obj是否相等
+    元素查询的操作：
+    Object get(Object key)：获取指定key对应的value
+    boolean containsKey(Object key)：是否包含指定的key
+    boolean containsValue(Object value)：是否包含指定的value
+    int size()：返回map中key-value对的个数
+    boolean isEmpty()：判断当前map是否为空
+    boolean equals(Object obj)：判断当前map和参数对象obj是否相等
   */
     @Test
     public void test4(){
@@ -137,10 +187,10 @@ boolean equals(Object obj)：判断当前map和参数对象obj是否相等
 
     /*
      添加、删除、修改操作：
- Object put(Object key,Object value)：将指定key-value添加到(或修改)当前map对象中
- void putAll(Map m):将m中的所有key-value对存放到当前map中
- Object remove(Object key)：移除指定key的key-value对，并返回value
- void clear()：清空当前map中的所有数据
+     Object put(Object key,Object value)：将指定key-value添加到(或修改)当前map对象中
+     void putAll(Map m):将m中的所有key-value对存放到当前map中
+     Object remove(Object key)：移除指定key的key-value对，并返回value
+     void clear()：清空当前map中的所有数据
      */
     @Test
     public void test3(){

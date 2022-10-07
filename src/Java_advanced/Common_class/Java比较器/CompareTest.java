d7b0a4eca705860969cb837fb46ab5a40814a55b
SxxStar <2291874995@qq.com>
2022-10-07 16:56:49 +0800

    JAVA比较器

---
+++ src/Java_advanced/Common_class/Java比较器/CompareTest.java
@@ -0,0 +1,50 @@
+package Java_advanced.Common_class.Java比较器;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 说明：Java中的对象，正常情况下，只能进行比较：==  或  != 。不能使用 > 或 < 的
+ *           但是在开发场景中，我们需要对多个对象进行排序，言外之意，就需要比较对象的大小。
+ *           如何实现？使用两个接口中的任何一个：Comparable 或 Comparator
+ *
+ *  二、Comparable接口与Comparator的使用的对比：
+ *     Comparable接口的方式一旦一定，保证Comparable接口实现类的对象在任何位置都可以比较大小。
+ *     Comparator接口属于临时性的比较。
+ *
+ *
+ * @author SHIXINXI
+ * @create 2022-10-07-15:55
+ */
+public class CompareTest {
+    /*
+        Compareable接口的使用举例：自然排序
+            1 像String、包装类等实现了Comparable接口，重写了compareTo(obj)方法，给出了比较两个对象大小的方式。
+            3. 重写compareTo(obj)的规则：
+                如果当前对象this大于形参对象obj，则返回正整数，
+                如果当前对象this小于形参对象obj，则返回负整数，
+                如果当前对象this等于形参对象obj，则返回零。
+            4. 对于自定义类来说，如果需要排序，我们可以让自定义类实现Comparable接口，重写compareTo(obj)方法。
+       在compareTo(obj)方法中指明如何排序
+    */
+    @Test
+    public void test1(){
+        String[] arr = new String[]{"cc","ee","bb","aa","ff","dd",};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    @Test
+    public void test2(){
+        Goods[] arr = new Goods[5];
+        arr[0] = new Goods("lenovoMouse",34);
+        arr[1] = new Goods("dellMouse",43);
+        arr[2] = new Goods("xiaomiMouse",12);
+        arr[3] = new Goods("huaweiMouse",65);
+        arr[4] = new Goods("microsoftMouse",43);
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
